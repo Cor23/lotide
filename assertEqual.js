ca72337d71fb89e523762daea5e2ed7dbb0da9b9
Corey <corey.cy.young@gmail.com>
2022-07-25 17:06:29 -0700

    Created an assertEqual function, added in some emojis

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
+const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(passEmoji + ' ' + 'Assertion Passed: ' + [actual] + ' === ' + [expected]);
+  } else {
+    console.log(failEmoji + ' ' + 'Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+  }
+};
+
+assertEqual('Lighthouse Labs', 'Lighthouse Labs');
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1, 2);
