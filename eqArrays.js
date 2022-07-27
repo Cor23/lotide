e2990f1e186fc562b6f58a2b02630cac85f49665
Corey <corey.cy.young@gmail.com>
2022-07-26 20:19:45 -0700

    Completed eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
+const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(inputOne, inputTwo) {
+  for(let i = 0; i < inputOne.length; i++) {
+    if(inputOne[i] !== inputTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  // => false
