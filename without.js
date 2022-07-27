c5c7c74eb87c972c52b59193481f15bb9abf1b54
Corey <corey.cy.young@gmail.com>
2022-07-27 12:47:54 -0700

    Start of without.js

---
+++ without.js
@@ -0,0 +1,27 @@
+const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
+const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
+
+const eqArrays = function(inputOne, inputTwo) {
+  for(let i = 0; i < inputOne.length; i++) {
+    if(inputOne[i] !== inputTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(inputOne, inputTwo) {
+  if(!eqArrays(inputOne, inputTwo)) {
+    console.log(`${failEmoji} Assertion Failed: ${inputOne} !== ${inputTwo}`)
+  } else if(eqArrays(inputOne, inputTwo)) {
+    console.log(`${passEmoji} Assertion Passed: ${inputOne} === ${inputTwo}`)
+  }
+};
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+
+//This function should take in a source array and a itemsToRemove array
+
+//It should return a new array with only those elements from source that are not present in the itemsToRemove array.
+
