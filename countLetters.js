b3ce23406d9feda5b119d31b582007add6c282ec
Corey <corey.cy.young@gmail.com>
2022-07-29 16:43:10 -0700

    Completed countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -14,11 +14,22 @@ const assertEqual = function(actual, expected) {
 //return a count of each of the letters in that sentence
 //For example, countLetters('LHL') return results indicating that L appears twice, and H once. {l: 2, h: 1}
 //Tips: You can use for...of loops with strings.
+//output: {L: 2, H: 3}
 
 
 let countLetters = function(string) {
-  let output = {}
-  console.log(string)
-}
+  let output = {};
+  for (let letter of string) {
+    if (letter !== ' ') {
+      output[letter] = 0;
+    }
+  }
+  for (let letter of string) {
+    if (letter !== ' ') {
+      output[letter] += 1;
+    }
+  }
+  return output;
+};
 
-countLetters("lighthouse in the house")
+console.log(countLetters("lighthouse in the house"));
