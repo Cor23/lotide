a3372858627260b08c8a1e4b7ecf726fafe1a44c
Corey <corey.cy.young@gmail.com>
2022-07-29 18:13:14 -0700

    Started letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
+const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
+
+const eqArrays = function(inputOne, inputTwo) {
+  for(let i = 0; i < inputOne.length; i++) {
+    if(inputOne[i] !== inputTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(inputOne, inputTwo) {
+  if(!eqArrays(inputOne, inputTwo)) {
+    console.log(`${failEmoji} Assertion Failed: ${inputOne} !== ${inputTwo}`)
+  } else if(eqArrays(inputOne, inputTwo)) {
+    console.log(`${passEmoji} Assertion Passed: ${inputOne} === ${inputTwo}`)
+  }
+};
+//---------------------------------------------------------//
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for(letter of sentence) {
+    if(letter !== ' ') {
+      results[letter] = [];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
