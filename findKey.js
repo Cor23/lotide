3062670a73746436c3d5cd0c0bd8caf57f06ad6b
Corey <corey.cy.young@gmail.com>
2022-07-31 15:23:56 -0700

    Implemented findKey function with tests

--- findKey.js
+++ findKey.js
@@ -18,15 +18,30 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(object, callback) {
 
-  
-
+  for (let key in object) {
+    const objectKeyValue = object[key];
+    const callBackAnswer = callback(objectKeyValue);
+    if (callBackAnswer) {
+      return key;
+    }
+  }
+  //for key in object
+  //get value and input into callback function callback returns true or false
+  //if true return key
+  //if false continue iteration
+  return undefined;
 };
 
-findKey({
+const dictionary = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}
+
+assertEqual(findKey(dictionary, x => x.stars === 2), "noma");
+assertEqual(findKey(dictionary, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(dictionary, x => x.stars !== 3), "Akelarre");
+assertEqual(findKey(dictionary, x => x.stars !== 3), "Blue Hill");
