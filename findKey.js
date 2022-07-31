0fbc13b3c894fe070921e2fb3a90c7e80e82516b
Corey <corey.cy.young@gmail.com>
2022-07-31 12:45:52 -0700

    Started findKey.js

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
+const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//----------------------------------------------------------------//
+
+//Implement the function findKey
+//takes in an object and a callback
+//scan the object and return the first key for which the callback returns a truthy value
+//If no key is found, return undefined
+
+const findKey = function(object, callback) {
+
+  
+
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
