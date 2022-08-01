5dbfb8beb4083a23ee70273fb38156b33d3929a0
Corey <corey.cy.young@gmail.com>
2022-08-01 14:26:42 -0700

    Removed test code, test code in test directory

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,4 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(inputOne, inputTwo) {
   for(let i = 0; i < inputOne.length; i++) {
@@ -17,10 +7,6 @@ const eqArrays = function(inputOne, inputTwo) {
     }
   }
   return true;
-}
+};
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  // => false
+module.exports = eqArrays;
