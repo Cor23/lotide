ec3401a362ee9aabb0eba9fdc36def425a1f252c
Corey <corey.cy.young@gmail.com>
2022-08-01 15:29:11 -0700

    Removed test code, test code in test directory

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,7 @@
+const eqObjects = require('./eqObjects');
+
 const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
 const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const eqArrays = function(inputOne, inputTwo) {
-  for (let i = 0; i < inputOne.length; i++) {
-    if (inputOne[i] !== inputTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const eqObjects = function (object1, object2) {        //input cd and dc into
-  const objectOneKeysArray = Object.keys(object1);    //["c", "d"]
-  const objectTwoKeysArray = Object.keys(object2);    //["d", "c"]
-  let lengthObjOne = objectOneKeysArray.length;       // 2
-  let lengthObjTwo = objectTwoKeysArray.length;       // 2
-
-  if (lengthObjOne === lengthObjTwo) {       // 2 === 2 true
-    return objectOneKeysArray.every(       // every loop passes key into predicate function
-      key => { //c //d
-        if (!object2.hasOwnProperty(key)) {  // false // false
-          return false;
-        }
-        if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // c: "1" false  // d: ["2", 3] true && d: ["2", 3] true
-          return eqArrays(object1[key], object2[key]);
-        }
-        return object1[key] === object2[key]
-      })   // c: '1' === c: '1' true
-  } else {
-    return false;
-  }
-
-};
-
-///---------------------------------------------------------
 
 const assertObjectsEqual = function (actual, expected) {
   const inspect = require('util').inspect;
@@ -46,7 +13,4 @@ const assertObjectsEqual = function (actual, expected) {
   }
 };
 
-const cd = { c: "2", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-
-assertObjectsEqual(cd, dc);
+module.exports = assertObjectsEqual;
