7178b7c679af3775ccd55be970e0610197a840d2
Corey <corey.cy.young@gmail.com>
2022-08-01 14:49:14 -0700

    Removed test code, test code in test directory

--- countOnly.js
+++ countOnly.js
@@ -1,20 +1,5 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-//------------------------------------------------------------------------//
-
 //The function should report back how many instances of each string were found in the allItems array of strings.
-
 // return an object that can represent the stats
-
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 
@@ -31,23 +16,6 @@ const countOnly = function(allItems, itemsToCount) {
   }
   console.log(results)
   return results;
-}
+};
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
