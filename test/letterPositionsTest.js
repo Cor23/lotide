3111253308ba4a764086e557a7ca7a60125ac8f1
Corey <corey.cy.young@gmail.com>
2022-08-01 15:33:23 -0700

    Created test file for letterPositions.js

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,5 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const letterPositions = require('../letterPositions');
+
+console.log(letterPositions("lighthouse in the house"));
