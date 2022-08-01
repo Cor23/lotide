e22a8c702991dc821ef9f210d2983450aaaad87a
Corey <corey.cy.young@gmail.com>
2022-08-01 13:50:49 -0700

    Removed test code, test code in test directory

--- tail.js
+++ tail.js
@@ -1,31 +1,6 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//------------ code above is a check for expected outcome --------------//
-
 const tail = function(array) {
-  return array.slice(1);
+  array = array.slice(1);
+  return array.join()
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], 'Lighthouse');
-assertEqual(result[1], 'Labs');
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const oneArray = [1];
-console.log(tail(oneArray), []);
-
-const emptyArray = [];
-console.log(tail(emptyArray), []);
+module.exports = tail;
