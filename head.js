ae52c84a261eeb72de5c85f193001eb9e9c8ea33
Corey <corey.cy.young@gmail.com>
2022-08-01 13:50:16 -0700

    Removed test code, test code in test directory

--- head.js
+++ head.js
@@ -1,16 +1,3 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//------------ code above is a check for expected outcome --------------//
-
 const head = function(array) {
   switch (array) {
   case undefined:
@@ -20,5 +7,4 @@ const head = function(array) {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+module.exports = head;
