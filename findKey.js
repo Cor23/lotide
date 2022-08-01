a83f1babe29cc97ff80b445f9532572f01831839
Corey <corey.cy.young@gmail.com>
2022-08-01 14:59:23 -0700

    Removed test code, test code in test directory

--- findKey.js
+++ findKey.js
@@ -1,16 +1,3 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//----------------------------------------------------------------//
-
 //Implement the function findKey
 //takes in an object and a callback
 //scan the object and return the first key for which the callback returns a truthy value
@@ -32,16 +19,4 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-const dictionary = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-assertEqual(findKey(dictionary, x => x.stars === 2), "noma");
-assertEqual(findKey(dictionary, x => x.stars === 3), "Akaleri");
-assertEqual(findKey(dictionary, x => x.stars !== 3), "Akelarre");
-assertEqual(findKey(dictionary, x => x.stars !== 3), "Blue Hill");
+module.exports = findKey;
