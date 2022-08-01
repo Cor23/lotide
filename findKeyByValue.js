45623391154e7eb4aeb9ad624356808741b6de48
Corey <corey.cy.young@gmail.com>
2022-08-01 14:12:57 -0700

    Removed test code, test code in test directory

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,40 +1,14 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//------------------------------^ Test Code ^-------------------------------//
-
-
 //Implement the function findKeyByValue which takes in an object and a value.
 //It should scan the object and return the first key which contains the given value.
 //If no key with that given value is found, then it should return undefined.
-
-const bestTVShowsByGenre = { 
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
 const findKeyByValue = function(object, value) {
   for (let genre in object) {
     if (object[genre] === value) {
       return genre;
-    } else {
-      return undefined;
     }
   }
+  return undefined;
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "romance");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), 'comedy');
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'comedy');
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'sciFi');
+module.exports = findKeyByValue;
