1098702a605b6029042a3c1adc61b68937c80ea9
Corey <corey.cy.young@gmail.com>
2022-08-01 15:48:23 -0700

    Removed test code, test code in test directory

--- middle.js
+++ middle.js
@@ -1,32 +1,7 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const eqArrays = function(inputOne, inputTwo) {
-  for(let i = 0; i < inputOne.length; i++) {
-    if(inputOne[i] !== inputTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(inputOne, inputTwo) {
-  if(!eqArrays(inputOne, inputTwo)) {
-    console.log(`${failEmoji} Assertion Failed: ${inputOne} !== ${inputTwo}`)
-  } else if(eqArrays(inputOne, inputTwo)) {
-    console.log(`${passEmoji} Assertion Passed: ${inputOne} === ${inputTwo}`)
-  }
-};
-
 //Implement middle which will take in an array and return the middle-most element(s) of the given array.
-
 //The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
-
 //For arrays with one or two elements, there is no middle. Return an empty array.
-
 //For arrays with odd number of elements, an array containing a single middle element should be returned.
-
 //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 
 const middle = function(array) {
@@ -51,14 +26,4 @@ const middle = function(array) {
   
 }
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+module.exports = middle;
