7e684412054d1f8ce7525b71d86c914f020ef057
Corey <corey.cy.young@gmail.com>
2022-08-01 14:26:07 -0700

    Removed test code, test code in test directory

--- countLetters.js
+++ countLetters.js
@@ -1,21 +1,8 @@
-const failEmoji = String.fromCodePoint(0x1F534, 0x1F534, 0x1F534);
-const passEmoji = String.fromCodePoint(0x1F7E2, 0x1F7E2, 0x1F7E2);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-//----------------------------------------------------------------------//
-
 //The function should take in a sentence(as a string)
 //return a count of each of the letters in that sentence
 //For example, countLetters('LHL') return results indicating that L appears twice, and H once. {l: 2, h: 1}
 //Tips: You can use for...of loops with strings.
 //output: {L: 2, H: 3}
-
 
 let countLetters = function(string) {
   let output = {};
@@ -32,4 +19,4 @@ let countLetters = function(string) {
   return output;
 };
 
-console.log(countLetters("lighthouse in the house"));
+module.exports = countLetters;
